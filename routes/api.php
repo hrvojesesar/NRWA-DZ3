b1bddb1e0b1260dc6488da618dbae4dba4de53a1
hrvojesesar <hrvojesesar007@gmail.com>
2023-05-26 21:08:43 +0200

    Testet RestAPI for table CustomerDemographics in Postman

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TerritoyControllerRestAPI;
+use App\Http\Controllers\CustomerDemographicControllerRestAPI;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::resource('territory', TerritoyControllerRestAPI::class);
+Route::resource('customerdemographic', CustomerDemographicControllerRestAPI::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
